2a93208a5d48a33d8bcc240f06c9a75b98ac863e
Pere <pere.alavedra@outlook.com>
2023-01-21 12:38:32 +0100

    BASIC and Advance Methods to create ASCI art.

--- src/main/java/org/IronSchool/Main.java
+++ src/main/java/org/IronSchool/Main.java
@@ -1,11 +1,19 @@
 package org.IronSchool;
+import org.IronSchool.ASCIIartTEST.ASCIIartTEST;
+import org.IronSchool.ASCIIartTEST.AdvancedASCIItest;
 
 public class Main {
     public static void main(String[] args) {
-        String name=InitialMenu.askingNameSchool();
-        int numberTeacher=InitialMenu.askedNumberTeachersCreated();
+        //String name=InitialMenu.askingNameSchool();
+        //int numberTeacher=InitialMenu.askedNumberTeachersCreated();
 
-        System.out.println(name);
-        System.out.println(numberTeacher);
+        //System.out.println(name);
+       // System.out.println(numberTeacher);
+
+        //si es estatico se puede llamar asi :
+        //ASCIIartTEST.metodoASSCIart();
+        AdvancedASCIItest.advancedASCIIMethod();
+
+
     }
 }
